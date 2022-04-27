deb12b6335507756ff3d4ff86b5c95c9c1898ddb
Rafael Tivane <luislangabusiness@gmail.com>
2022-04-27 19:41:32 +0200

    implement POST method and save todo

--- src/routes/todos/index.js
+++ src/routes/todos/index.js
@@ -3,8 +3,14 @@ const { db } = require("../../connections/mongodb")
 
 todosRouter.get("/todos", async (req, res) => {
     const result = await db().find({ }).toArray();
-    res.send({ todos: [], result});
-})
+    res.send({ todos: result});
+});
+
+todosRouter.post("/todos", async (req, res) => {
+    const todo = req.body;
+    await db().insert(todo)
+    res.status(204).send();
+});
 
 module.exports = {
     todosRouter
